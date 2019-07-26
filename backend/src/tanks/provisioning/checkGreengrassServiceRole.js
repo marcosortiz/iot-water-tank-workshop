91326063752876b91b58e51f097e9b64893dc8eb
Jon Slominski <jonslo@amazon.com>
2019-07-26 15:18:15 -0500

    fix formatting

--- backend/src/tanks/provisioning/checkGreengrassServiceRole.js
+++ backend/src/tanks/provisioning/checkGreengrassServiceRole.js
@@ -6,19 +6,19 @@ var Greengrass = require('aws-sdk/clients/greengrass');
 AWS.config.region = process.env.AWS_REGION;
 var greengrass = new Greengrass();
 
-module.exports =  {
+module.exports = {
     checkGreengrassServiceRole: function (event, context, cb) {
         var params = {};
-        greengrass.getServiceRoleForAccount(params, function(err, data) {
+        greengrass.getServiceRoleForAccount(params, function (err, data) {
             if (err) {
-                if( err.code === 'UnknownError' && err.message === 'You need to attach an IAM role to this AWS account.') {
-                    data = {exists: false}
+                if (err.code === 'UnknownError' && err.message === 'You need to attach an IAM role to this AWS account.') {
+                    data = { exists: false }
                     cb(null, data)
                 } else {
                     cb(err, null);
                 }
             } else if (err === null) {
-                data = {exists: true}
+                data = { exists: true }
                 cb(null, data)
             }
         });
