131eb848cd59eb62414507db0e95a6c8b2d4353c
Marcos Ortiz <marcos.ortiz@icloud.com>
2019-09-16 16:32:55 -0500

    Adding region param to script.

--- frontend/bin/users/create-users.sh
+++ frontend/bin/users/create-users.sh
@@ -3,5 +3,6 @@
 # Load config
 mydir="${0%/*}"
 USER_POOL_ID=$(cat "$mydir"/../../src/config/config.json | jq -r '.UserPoolId')
+BACKEND_REGION=${REGION:-$(cat "$mydir"/../../src/config/config.json | jq -r '.region')}
 
-aws cognito-idp admin-create-user --user-pool-id $USER_POOL_ID --username <email>
+aws cognito-idp admin-create-user --user-pool-id $USER_POOL_ID --username <email> --region $BACKEND_REGION
